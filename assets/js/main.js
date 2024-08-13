f5859a07204a1d1921c69d7effce77ba080155a4
Robert <maculewicz.au@gmail.com>
2024-08-13 23:33:20 +1000

    fix_hamburger_menu_

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,10 @@
-console.log("Welcome")
+
+
+// add function close hamburger menu after click link
+document.addEventListener('click', function (event) {
+    if (event.target.matches('.nav-link')) {
+        var navbarCollapse = document.querySelector('.navbar-collapse');
+        var bsCollapse = new bootstrap.Collapse(navbarCollapse);
+        bsCollapse.hide();
+    }
+});
