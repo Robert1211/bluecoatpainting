4b517289a3b959ffcb5e4e871d5f0d41810b67f6
Robert <maculewicz.au@gmail.com>
2024-08-25 19:54:58 +1000

    setup_collaps_nav

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,11 +15,20 @@ $(document).ready(function () {
     $(window).scroll(function () {
         var scroll = $(window).scrollTop();
         if (scroll > 100) {
-            $(".navbar").css("background", "rgba(218, 218, 218, .4)");
+            $(".navbar").css("background", "rgba(192, 192, 192, .2)");
             $(".navbar").css("transition", "1.5s");
+            $(".navbar").css("padding-left", "269px");
+            $(".navbar").css("padding-right", "269px");
+
+
+
 
         } else {
             $(".navbar").css("background", "transparent");
+            $(".navbar").css("padding-left", "269px");
+            $(".navbar").css("padding-right", "269px");
+
+
         }
     });
 });
