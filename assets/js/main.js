87b8977abdea68ddc8d5c547daa9bf752e521bcb
Robert <maculewicz.au@gmail.com>
2024-09-07 18:06:59 +1000

    copy_text_info_add_logo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,42 +1,33 @@
-console.log("Robert")
+console.log("Robert");
 
 // add function close hamburger menu after click link
-document.addEventListener('click', function (event) {
-    if (event.target.matches('.nav-link')) {
-        var navbarCollapse = document.querySelector('.navbar-collapse');
-        var bsCollapse = new bootstrap.Collapse(navbarCollapse);
-        bsCollapse.hide();
-    }
+document.addEventListener("click", function (event) {
+  if (event.target.matches(".nav-link")) {
+    var navbarCollapse = document.querySelector(".navbar-collapse");
+    var bsCollapse = new bootstrap.Collapse(navbarCollapse);
+    bsCollapse.hide();
+  }
 });
-
 
 //function for when scrolling background nav change color
 $(document).ready(function () {
-    $(window).scroll(function () {
-        var scroll = $(window).scrollTop();
-        if (scroll > 100) {
-            $(".navbar").css("background", "rgba(192, 192, 192, .2)");
-            $(".navbar").css("backdrop-filter", "blur(10px)");
-            $(".navbar").css("transition", "1.5s");
-            // $(".navbar").css("padding-left", "269px");
-            // $(".navbar").css("padding-right", "269px");
-
-
-
-
-
-        } else {
-            $(".navbar").css("background", "transparent");
-            // $(".navbar").css("padding-left", "269px");
-            // $(".navbar").css("padding-right", "269px");
-
-
-        }
-    });
+  $(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+    if (scroll > 100) {
+      $(".navbar").css("background", "rgba(192, 192, 192, .2)");
+      $(".navbar").css("backdrop-filter", "blur(10px)");
+      $(".navbar").css("transition", "1.5s");
+      // $(".navbar").css("padding-left", "269px");
+      // $(".navbar").css("padding-right", "269px");
+    } else {
+      $(".navbar").css("background", "transparent");
+      // $(".navbar").css("padding-left", "269px");
+      // $(".navbar").css("padding-right", "269px");
+    }
+  });
 });
 
-
-// different way for function color background 
+// different way for function color background
 // window.onscroll = function () {
 //     var navbar = document.getElementById("navbar");
 //     if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
